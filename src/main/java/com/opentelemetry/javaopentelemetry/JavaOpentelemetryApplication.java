0dce9c8fd2f888aa1e96612456223a276fc7d59d
diegodias <emerson.diego.duarte@gmail.com>
2024-08-25 17:06:58 -0300

    java open telemetry

--- src/main/java/com/opentelemetry/javaopentelemetry/JavaOpentelemetryApplication.java
+++ src/main/java/com/opentelemetry/javaopentelemetry/JavaOpentelemetryApplication.java
@@ -1,13 +1,31 @@
 package com.opentelemetry.javaopentelemetry;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class JavaOpentelemetryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaOpentelemetryApplication.class, args);
+    }
+
+    // Unregister the OpenTelemetryMeterRegistry from Metrics.globalRegistry and make it available
+    // as a Spring bean instead.
+    @Bean
+    @ConditionalOnClass(name = "io.opentelemetry.javaagent.OpenTelemetryAgent")
+    public MeterRegistry otelRegistry() {
+        Optional<MeterRegistry> otelRegistry = Metrics.globalRegistry.getRegistries().stream()
+                .filter(r -> r.getClass().getName().contains("OpenTelemetryMeterRegistry"))
+                .findAny();
+        otelRegistry.ifPresent(Metrics.globalRegistry::remove);
+        return otelRegistry.orElse(null);
     }
 
 }
--- src/main/java/com/opentelemetry/javaopentelemetry/RestController.java
+++ src/main/java/com/opentelemetry/javaopentelemetry/RestController.java
@@ -1,7 +1,9 @@
 package com.opentelemetry.javaopentelemetry;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,11 +15,27 @@ public class RestController {
 
     private final UserRepository userRepository;
 
+    private final MeterRegistry registry;
+
     @GetMapping(value = "/api")
-    public String api() throws InterruptedException {
+    public ResponseEntity<Object> api() throws InterruptedException {
         log.info("api");
-        userRepository.findAll().forEach(System.out::println);
+        registry.counter("greetings.total", "name", "api").increment();
+        return ResponseEntity.ok().body(userRepository.findAll());
+    }
+
+
+    @GetMapping(value = "/api/v2")
+    public ResponseEntity<Object> api1() throws InterruptedException {
         log.info("api 2");
-        return "Hello World!";
+        registry.counter("greetings.total", "name", "api2").increment();
+        return ResponseEntity.ok().body(userRepository.findAll());
+    }
+
+    @GetMapping(value = "/api/v3")
+    public ResponseEntity<Object> api3() throws InterruptedException {
+        log.info("api 3");
+        registry.counter("greetings.total", "name", "api3").increment();
+        throw new RuntimeException("Error");
     }
 }
